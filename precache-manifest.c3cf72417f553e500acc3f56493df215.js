bdeeb2986c13849793688753d640db5e5a79ea1e
Emma Paris <eparis@atlassian.com>
2020-05-05 01:00:42 -0400

    Updates

---
+++ precache-manifest.c3cf72417f553e500acc3f56493df215.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5b86c1d80366cda9e72f97a0233b87d",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "fb2ed251d5e4bb56b03d",
+    "url": "/deploy-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f0c3e50004c589ee95aa",
+    "url": "/deploy-react/static/js/2.c09bbabb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-react/static/js/2.c09bbabb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb2ed251d5e4bb56b03d",
+    "url": "/deploy-react/static/js/main.673f1b5f.chunk.js"
+  },
+  {
+    "revision": "13314ac4dfa3af18334d",
+    "url": "/deploy-react/static/js/runtime-main.86a71034.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
